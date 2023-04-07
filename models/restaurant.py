38555a9848310f54c5ee69763280a72b09adb35b
DaFi02 <diego.andia.f@gmail.com>
2023-04-06 20:55:48 -0500

    More

--- models/restaurant.py
+++ models/restaurant.py
@@ -28,22 +28,34 @@ class Dish(db.Model):
     description = db.Column(db.String)
     cost = db.Column(db.Float)
     price = db.Column(db.Float)
+    day = db.Column(db.String)
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
     restaurant = db.relationship("Restaurant", back_populates="dishes")
 
-    def __init__(self, name, description, cost, price, restaurant_id):
+    def __init__(self, name, description, cost, price, day, restaurant_id):
         self.name = name
         self.description = description
         self.cost = cost
         self.price = price
+        self.day = day
         self.restaurant_id = restaurant_id
     
-    def to_JSON(self):
+    def to_JSON_to_resturant(self):
         return {
             'id': self.id,
             'name': self.name,
             'description': self.description,
             'cost': self.cost,
+            'price': self.price,
+            'restaurant_id': self.restaurant_id,
+            'day': self.day
+        }
+    
+    def to_JSON_to_cliente(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
             'price': self.price,
             'restaurant_id': self.restaurant_id
         }
--- routes/dishes.py
+++ routes/dishes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request, redirect, url_for, flash
 from utils.conection import db
-from models.restaurant import Dish
+from models.restaurant import Dish, Restaurant
+from datetime import datetime
 
 dishes = Blueprint('dishes', __name__)
 
@@ -8,7 +9,17 @@ dishes = Blueprint('dishes', __name__)
 @dishes.route('/dishes', methods=['GET'])
 def get_dishes():
     dishes = Dish.query.all()
-    return jsonify({'dishes': [dish.to_JSON() for dish in dishes]})
+    return jsonify({'dishes': [dish.to_JSON_to_resturant() for dish in dishes]})
+
+
+
+@dishes.route('/dishes/<restaurant_id>/client', methods=['GET'])
+def get_dishes_client(restaurant_id):
+    day = datetime.now().strftime("%A")
+    day = str.lower(day)
+    dishes = Dish.query.filter_by(day=day, restaurant_id=restaurant_id)
+    name_restaurant = Restaurant.query.filter_by(id=restaurant_id).first().name
+    return jsonify({'dishes'+ " of "+name_restaurant : [dish.to_JSON_to_cliente() for dish in dishes]})
 
 
 @dishes.route('/dishes', methods=['POST'])
@@ -17,8 +28,9 @@ def add_dish():
     description = request.json['description']
     cost = request.json['cost']
     price = request.json['price']
+    day = str.lower(request.json['day'])
     restaurant_id = request.json['restaurant_id']
-    new_dish = Dish(name, description, cost, price, restaurant_id)
+    new_dish = Dish(name, description, cost, price, day, restaurant_id)
 
     db.session.add(new_dish)
     db.session.commit()
@@ -31,6 +43,5 @@ def get_dish_of_restaurant(restaurant_id):
     dish = Dish.query.filter_by(restaurant_id=restaurant_id)
     if not dish:
         return jsonify({'message': 'Dish not found'}), 404
-    for a in dish:
-        print(a.to_JSON())
-    return jsonify({'dishes': [dish.to_JSON() for dish in dish]})
+    
+    return jsonify({'dishes': [dish.to_JSON_to_resturant() for dish in dish]})
